2f25476437853eeb751b6dce148ec71b2eafe036
brunoalves01 <brutavinho@gmail.com>
2026-09-16 15:00:44 -0300

    Meu projeto backend

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,84 @@
 // Importa a biblioteca Express e também o tipo Express
 // O Express será utilizado para criar o servidor web
 import express from "express";
-import type { Express } from "express";
+import type { Express, Request, Response } from "express";
+
+/*Importar a classe Player do arquivo Player.ts
+Por que Player.ts deve ser importado com a extensão .js? Porque o TypeScript compila para JavaScript,
+gera arquivos .js. Portanto, ao importar arquivos Typescript, você deve usar a extensão .js 
+Para que o Node.js consiga localizar o arquivo corretamente.*/
+import { Player } from "./models/Player.js";
 
 // Cria uma aplicação Express
 // A função express() devolve um objeto que representa o servidor da aplicação
 const app: Express = express();
 
+//Middleware para permitir que o servidor aceite requisições com corpo em formato JSON
+app.use(express.json());
+
 // Define a porta onde o servidor ficará disponível
 // Neste caso, o servidor poderá ser acessado pela porta 8081
 const PORT: number = 8081;
+
+const player = new Player("Brunão", 100, 1);
+
+/// Rota GET para obter a informação do player
+/*Quando um usuario acessar a rota "/player" via método GET
+O servidor irá retornar os dados do Player e devolverá as informações em formato JSON como resposta.*/
+app.get("/player", (req: Request, res: Response) => {
+    res.json({ 
+        message: "Informações do Player",
+        player: player,
+    });    
+});
+
+/*Rota POST para simular um ataque do player
+Quando um usuario acessar a rota "/player/attack" via método POST
+O servidor irá chamar o método attack() do Player e devolverá a mensagem de ataque como resposta.*/
+app.post("/player/attack", (req: Request, res: Response) => {
+    //Chama o método attack() do player e armazena a mensagem retornada
+    const AttackMessage = player.attack();//
+    res.json({ 
+        message: AttackMessage,
+    });
+});
+
+// Rota POST para simular o player recebendo dano
+/*Quando um usuario acessar a rota "/player/damage" via método POST
+O servidor irá chamar o método takeDamage() do Player e devolverá a mensagem de dano como resposta.*/
+app.post("/player/damage", (req: Request, res: Response) => {
+    //Chama o método takeDamage() do player e armazena a mensagem retornada
+    const { damage } = req.body;
+    const DamageMessage = player.takedamage(damage);
+    res.json({ 
+        //Retornar a mensagem de dano, a saúde atual e o nível atual do player
+        action: DamageMessage,
+        //Retorna a saúde atual do player
+        currentHealth: player.health,
+        //Retorna o nível atual do player
+        currentLevel: player.level,
+    });
+});
+    app.post("/player/heal", (req: Request, res: Response) => {
+        const { heal } = req.body;
+        const HealMessage = player.heal(heal);
+        res.json({
+            action: HealMessage,
+            currentHealth: player.health,
+            currentLevel: player.level,
+        });
+    });
+
+
+
 
 // Inicializa o servidor utilizando a porta definida
 // O método listen() faz o servidor começar a "escutar" requisições HTTP
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
+    console.log("Rotas Disponíveis:");
+    console.log(`GET http://localhost:${PORT}/player - Obter informações do player`);
+    console.log(`POST http://localhost:${PORT}/player/attack - Simular ataque do player`);
+    console.log(`POST http://localhost:${PORT}/player/damage - Simular dano recebido pelo player`);
+    console.log(`POST http://localhost:${PORT}/player/heal - Simular cura do player`);
 });
---
+++ src/models/Player.ts
@@ -0,0 +1,55 @@
+/*
+A palavra-chave "export" é utilizada para exportar a classe Player,
+Permitindo que ela seja importada e utilizada em outros arquivos do projeto.
+A palavra-chave "class" é utilizada para definir uma classe em TypeScript.
+*/
+export class Player {
+    //Atributos da classe Player:
+    //A palavra-chave "public" é utilizada para definir propriedades públicas da classe,
+    //Que podem ser acessadas de fora da classe.
+    public name: string; //nome do player (texto)
+    public health: number; //pontos de vida do player (numero)
+    public level: number; //nível do player (numero)
+
+    //Construtor da classe Player:
+    /* o construtor é um método especial que é chamado quando
+    uma nova instância da classe é criada.*/
+    constructor(name: string, health: number = 100, level: number = 1) {
+        /*A palavra-chave "this" é utilizada para referenciar a instância atual da classe. Ou seja,
+        *Pegue o atributo "health" da classe Player e atribua o valor de `health´= 100 para ele.*/
+        this.name = name; //Inicializa o atributo name.
+        this.health = health; //Inicializa o atributo health com o valor introduzido.
+        this.level = level; //Inicializa o atributo level com o valor introduzido.
+    }
+
+    //Método da classe Player:
+    /*Métodos são funções que pertencem a uma classe e podem ser chamadas em instâncias dessa classe.*/
+    //O método "attack" é utilizado para simular um ataque do player. Reduzindo sua saúde
+    public attack(): string {
+        //Calcula o dano causado pelo ataque com base no nível do player.
+        const damage = this.level * 10; 
+        //A palavra-chave "return" é utilizada para devolver um valor de uma função ou método.
+        return `O player ${this.name} atacou causando ${damage} de dano!`;
+    }
+
+    //O método "takeDamage" é utilizado para simular o dano recebido pelo player. Reduzindo a saúde do player.
+    public takedamage(damage: number): string {
+        //Reduz a saúde do player com base no dano recebido.
+        this.health -= damage; 
+        //Verifica se a saúde do player caiu para 0 ou menos.
+        if (this.health <= 0) {
+            this.health = 0; //Garante que a saúde não fique negativa.
+            return `O player ${this.name} foi derrotado!`;
+        }
+        return `O player ${this.name} recebeu ${damage} de dano e agora tem ${this.health} de vida.`;
+    }
+
+    public heal(heal: number): string {
+        this.health += heal;  
+        if (this.health >= 100) {
+            this.health = 100; // Garante que a saúde não ultrapasse 100.
+            return `O player ${this.name} foi curado em ${heal} pontos de vida e agora tem ${this.health} de vida.`;
+        }
+        return `O player ${this.name} foi curado em ${heal} pontos de vida e agora tem ${this.health} de vida.`;
+    }
+}
